757ef9dc33de675ec962565ca041ea3327f1fada
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-04-24 11:14:27 +0500

    finished

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateBookDto {
+  @ApiProperty({ type: String })
   name: string;
+
+  @ApiProperty({ type: Number })
   year: number;
+
+  @ApiProperty({ type: Number })
   user: number;
+
+  @ApiProperty({ type: Array, example: [1, 2, 3] })
   genre: number[];
 }
